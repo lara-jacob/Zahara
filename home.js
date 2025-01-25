ab99c4f6cb09cd5eb60d15c62ff0a0024faba67f
RiziaPrabin <riziaprabin1910@gmail.com>
2025-01-26 01:27:08 +0530

    applied navigation

---
+++ home.js
@@ -0,0 +1,9 @@
+document.getElementById("Explore").addEventListener("click", () => {
+    // Navigate to signin.html
+    window.location.href = "scheme.html";
+});
+
+document.getElementById("login").addEventListener("click", () => {
+    // Navigate to signin.html
+    window.location.href = "login.html";
+});
